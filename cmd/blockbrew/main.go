840756903ce4eef11b81ce1d2b031d6fd4627535
Max <maxtclinton@gmail.com>
2026-03-08 09:20:43 -0400

    add datadir flag and version output to main

--- cmd/blockbrew/main.go
+++ cmd/blockbrew/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("blockbrew - Bitcoin full node in Go")
+	home, err := os.UserHomeDir()
+	if err != nil {
+		home = "."
+	}
+	defaultDatadir := filepath.Join(home, ".blockbrew")
+
+	datadir := flag.String("datadir", defaultDatadir, "data directory for blockchain and wallet")
+	flag.Parse()
+
+	fmt.Println("blockbrew v0.1.0 starting...")
+	fmt.Println("datadir:", *datadir)
 }
